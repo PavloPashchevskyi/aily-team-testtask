0210c755f0543905a5a62de5103f07cfb241afed
Pavlo Pashchevskyi <pavlo.pashchevskyi@idapgroup.com>
2018-06-25 13:41:20 +0300

    Migrations for creating/dropping the entity of "Link" have been created

---
+++ app/DoctrineMigrations/Version0001_Create_Link_Entity.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version0001_Create_Link_Entity extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE link (id INT AUTO_INCREMENT NOT NULL, original VARCHAR(255) NOT NULL, short VARCHAR(255) NOT NULL, lifetime_to DATETIME DEFAULT NULL, active TINYINT(1) DEFAULT \'0\' NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE link');
+    }
+}
