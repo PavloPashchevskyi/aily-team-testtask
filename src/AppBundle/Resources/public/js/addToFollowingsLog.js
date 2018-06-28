38dbfa68f7c703a7a719329d5a84ace94a9de96a
Pavlo Pashchevskyi <pavlo.pashchevskyi@idapgroup.com>
2018-06-28 20:13:45 +0300

    File with ajax Query to FollowingLinksController

--- src/AppBundle/Resources/public/js/addToFollowingsLog.js
+++ src/AppBundle/Resources/public/js/addToFollowingsLog.js
@@ -2,10 +2,14 @@ $(function () {
     $('a[data-shortlink="true"]').click(function() {
         ajaxObject = {
             type: "POST",
-            url: '/followings-log/add',
+            url: "http://aily-team-testtask.loc/app_dev.php/followings-log/add",
             dataType: "json",
+            data: {
+                link: $(this).attr('href'),
+                isShort: $(this).attr('data-shortlink')
+            },
             success: function (response) {
-
+                window.location.href = response['original'];
             },
             error: function () {
                 console.log('AjaX ERROR!!!');
